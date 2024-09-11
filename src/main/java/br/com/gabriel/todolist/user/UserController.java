7780ed79a08f5a272918a0a2e8c182860cca1630
Gabriel <gabriel.script@gmail.com>
2024-09-10 23:52:47 -0300

    add create user in database

--- src/main/java/br/com/gabriel/todolist/user/UserController.java
+++ src/main/java/br/com/gabriel/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.gabriel.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository repository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-    System.out.println("Teste: " + userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.repository.save(userModel);
+
+        return userCreated;
     }
 }
