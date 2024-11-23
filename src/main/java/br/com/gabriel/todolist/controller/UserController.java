ee3bf2636078c5714bbc63357738e9932e68e094
gabrielScript-dev <gabriel.script@gmail.com>
2024-11-23 17:37:52 -0300

    updating controller

--- src/main/java/br/com/gabriel/todolist/controller/UserController.java
+++ src/main/java/br/com/gabriel/todolist/controller/UserController.java
@@ -1,4 +1,15 @@
 package br.com.gabriel.todolist.controller;
 
+import br.com.gabriel.todolist.model.User;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody User user) {
+        System.out.println(user.getName());
+    }
 }
