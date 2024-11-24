1952f590e2cc980e6b1943ec8751ede675ce78a3
gabrielScript-dev <gabriel.script@gmail.com>
2024-11-24 16:09:56 -0300

    encrypted password user.

--- src/main/java/br/com/gabriel/todolist/controller/UserController.java
+++ src/main/java/br/com/gabriel/todolist/controller/UserController.java
@@ -1,15 +1,34 @@
 package br.com.gabriel.todolist.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.gabriel.todolist.model.User;
-import lombok.Getter;
+import br.com.gabriel.todolist.repository.IUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody User user) {
-        System.out.println(user.getName());
+    public ResponseEntity<User> create(@RequestBody User user) {
+
+        User findByUserName = userRepository.findByUserName(user.getUserName());
+
+        if(findByUserName != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        String encryptedPassword = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+        user.setPassword(encryptedPassword);
+
+        User userCreated = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
