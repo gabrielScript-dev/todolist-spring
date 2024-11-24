df9a275672011be664c0ebfcf7a85aa0089514da
gabrielScript-dev <gabriel.script@gmail.com>
2024-11-24 16:30:17 -0300

    added taskList route.

--- src/main/java/br/com/gabriel/todolist/controller/TaskController.java
+++ src/main/java/br/com/gabriel/todolist/controller/TaskController.java
@@ -6,12 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -40,5 +38,12 @@ public class TaskController {
         repository.save(task);
 
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<Task> taskList(HttpServletRequest request) {
+        UUID userId = (UUID) request.getAttribute("idUser");
+
+        return repository.findByIdUser(userId);
     }
 }
--- src/main/java/br/com/gabriel/todolist/repository/ITaskRepository.java
+++ src/main/java/br/com/gabriel/todolist/repository/ITaskRepository.java
@@ -3,8 +3,9 @@ package br.com.gabriel.todolist.repository;
 import br.com.gabriel.todolist.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<Task, UUID> {
-
+    List<Task> findByIdUser(UUID idUser);
 }
